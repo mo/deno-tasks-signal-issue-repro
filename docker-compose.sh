757486e817bd307b558d5e099da4a8eed1ccbc5e
Martin Olsson <martin@minimum.se>
2024-11-13 10:21:25 +0100

    more testing commands

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+
+trap sigint SIGINT
+function sigint() {
+    echo
+    echo "got SIGINT"
+    echo
+}
+
+trap sigint SIGTERM
+function sigterm() {
+    echo
+    echo "got SIGTERM"
+    echo
+}
+
+
 docker compose -f compose.yaml up
